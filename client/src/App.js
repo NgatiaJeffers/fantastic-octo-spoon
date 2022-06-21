87ad5ec57ea505ff19eb96fab2571486e3bf285b
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-06-21 10:15:03 +0300

    Add classes for css

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from "./images/memories.png";
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container>
-            <AppBar position="static" color="default">
-                <Typography variant="h2" align="center" color="inherit">Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="default">
+                <Typography className={classes.heading} variant="h2" align="center" color="inherit">Memories</Typography>
+                <img classes={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in={true}>
                 <Container>
