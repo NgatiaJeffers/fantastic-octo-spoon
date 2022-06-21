38da141b6bc30c24275af3df21226d79d42f877f
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-06-21 10:04:03 +0300

    Components styling and structuring

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from "./images/memories.png";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <Container>
+            <AppBar position="static" color="default">
+                <Typography variant="h2" align="center" color="inherit">Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
+            </AppBar>
+            <Grow in={true}>
+                <Container>
+                    <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
---
+++ server/models/postMessage.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import moongose from 'mongoose';
+
+const postSchema = moongose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
